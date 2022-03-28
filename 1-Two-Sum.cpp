1b9113b515f8247e5a9027881141061777ab75e3
Mohammad Tarique Anjum <69107862+iamtariqueanjum@users.noreply.github.com>
2022-03-28 14:58:51 +0530

    Update 1-Two-Sum.cpp

--- 1-Two-Sum.cpp
+++ 1-Two-Sum.cpp
@@ -26,3 +26,22 @@ public:
         return pos;
     }
 };
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> pos;
+        map<int,int> mp;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            if(mp.count(target-nums[i])){
+                pos.push_back(mp[target-nums[i]]);
+                pos.push_back(i);
+                break;
+            }
+            else
+                mp[nums[i]] = i;
+        }
+        return pos;
+    }
+};
