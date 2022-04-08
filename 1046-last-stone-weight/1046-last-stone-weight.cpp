d3f7e3318a40661b61c7414b7fa357726b89a30f
Mohammad Tarique Anjum <69107862+iamtariqueanjum@users.noreply.github.com>
2022-04-08 22:15:32 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (79.44%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        int n = stones.size();
+        for(int i = 0; i < n; i++){
+            pq.push(stones[i]);
+        }
+        while(pq.size()>1){
+            int y = pq.top();
+            pq.pop();
+            int x = pq.top();
+            pq.pop();
+            if(x != y){
+                pq.push(y-x);
+            }
+        }
+        if(pq.size()==1){
+            return pq.top();
+        }
+        return 0;
+    }
+};
